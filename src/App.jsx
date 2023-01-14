58fde7d1af5a6d8edce3c5518114db05fd429424
yjyan21 <yjyan1987@gmail.com>
2023-01-13 21:05:07 -0800

    add ant design

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./assets/base.css"
+import {Button} from 'antd'
+
+export default function App() {
+  return (
+    <div>
+      <Button type="primary">Button</Button>
+			App
+    </div>
+  )
+}
