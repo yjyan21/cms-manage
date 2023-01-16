a2f29e3cee1c87d8b6b2f80d4dee161a626004cd
yjyan21 <yjyan1987@gmail.com>
2023-01-15 20:57:13 -0800

    change bgcolor

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import "./assets/base.less"
-import {Button} from 'antd'
 import {Outlet} from 'react-router-dom'
 
 export default function App() {
   return (
     <div>
-      <Button type="primary">Button</Button>
+      App
 			<Outlet />
     </div>
   )
